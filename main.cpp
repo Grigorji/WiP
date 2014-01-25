82e3c0a19d9e49b8077d598b23c8745aa20c81c9
grzegorz wojcikiewicz <wojcikiewiczgrzegorz@gmail.com>
2014-01-25 12:12:46 +0100

    modifying definition of table and output text

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main()
 {
-    int tab = new int(10);
+	int *tab;
     cout << "Start Exercise number 1 in group" << endl;
     tab=generate();
     for(int i=0; i<10; i++)
     {
-                cout<<"Element: ["<<i<<"]: "<<tab[i];
+                cout<<"Element: ["<<i+1<<"]: "<<tab[i]<<endl;
     }
     cout<<"\nEnd of Program";
+	system("pause");
     return 0;
 }
