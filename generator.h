cd1d3d3f0b4ec07cf953f54ee5dfc905c8566565
grzegorz wojcikiewicz <wojcikiewiczgrzegorz@gmail.com>
2014-01-25 12:38:20 +0100

    all function moved to utils.h and deleted unused files

--- generator.h
+++
@@ -1,9 +0,0 @@
-#include <random>
-
-int* generate() {
-	int* tablica = new int[10];
-	for (int i = 0; i < 10; i++){
-		tablica[i] = rand() % 100;
-	}
-	return tablica;
-}
--- utils.h
+++ utils.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <random>
 
 void show(int *tab){
 	for (int i = 0; i < 10; i++){
@@ -12,4 +13,44 @@ int sum(int *tab){
 		result += tab[i];
 	}
 	return result;
+}
+
+int* generate() {
+	int* tablica = new int[10];
+	for (int i = 0; i < 10; i++){
+		tablica[i] = rand() % 100;
+	}
+	return tablica;
+}
+
+int min(int* wsk)
+{
+	int min;
+	std::cout << "Function MIN" << std::endl;
+	for (int i = 0; i<sizeof(wsk) / sizeof(*wsk); i++)
+	{
+		if (i>i + 1){
+			min = i + 1;
+		}
+		else{
+			min = i;
+		}
+	}
+	return min;
+}
+
+int max(int* wsk)
+{
+	int max;
+	std::cout << "Function MAX" << std::endl;
+	for (int i = 0; i<sizeof(wsk) / sizeof(*wsk); i++)
+	{
+		if (i>i + 1){
+			max = i + 1;
+		}
+		else{
+			max = i;
+		}
+	}
+	return max;
 }
