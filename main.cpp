574b7a035bf9e03559ec4b47f66b7fb6f1a23389
Michał Żegliński <michal.zegl@gmail.com>
2014-01-25 12:41:23 +0100

    Change output. MAIN.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
-#include "generator.h"
 #include "utils.h"
 
 using namespace std;
 
 int main()
 {
-	int *tab;
-    cout << "Start Exercise number 1 in group" << endl;
+	int *tab,value_min,value_max,value_sum;
+    cout << "Start Exercise number 1 2 3 4 in group" << endl;
     tab=generate();
 	show(tab);
+	value_min= min(tab);
+	value_max= max(tab);
+	value_sum=sum(tab);
+	cout<<"\nMIN= "<<value_min<<endl;
+	cout<<"\nMAX= "<<value_max<<endl;
+	cout<<"\nSUM= "<<value_sum<<endl;
     cout<<"\nEnd of Program";
 	system("pause");
     return 0;
