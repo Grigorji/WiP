788737547cf0e0c03b6984f7a16f5a392794d844
grzegorz wojcikiewicz <wojcikiewiczgrzegorz@gmail.com>
2014-01-25 12:29:49 +0100

    created function sum()

--- utils.h
+++ utils.h
@@ -5,3 +5,11 @@ void show(int *tab){
 		std::cout << "Element: [" << i + 1 << "]: " << tab[i] << std::endl;
 	}
 }
+
+int sum(int *tab){
+	int result;
+	for (int i = 0; i < 10; i++){
+		result += tab[i];
+	}
+	return result;
+}
